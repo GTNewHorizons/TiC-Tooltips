7f340778c3e2e3573bde332c6b6dd3043c2a0375
Johann Bernhardt <johann.bernhardt@tum.de>
2021-12-02 18:08:13 +0100

    Migrated to unified build script

--- src/main/java/squeek/tictooltips/ModInfo.java
+++ src/main/java/squeek/tictooltips/ModInfo.java
@@ -3,5 +3,6 @@ package squeek.tictooltips;
 public final class ModInfo
 {
 	public static final String MODID = "TiCTooltips";
-	public static final String VERSION = "${version}";
+	public static final String MODNAME = "TiC Tooltips";
+	public static final String VERSION = "GRADLETOKEN_VERSION";
 }
